fbc3e04bd23237b9cad1b8492e0ff3b34cb49a49
royhutw <royhutw@gmal.com>
2025-04-17 00:37:40 +0800

    tm00.py

--- tm00.py
+++ tm00.py
@@ -30,43 +30,33 @@ user.inBoundary = internet
 user.levels = [0,1]
 user.isAdmin = False
 
-httpd = Server("IIS8.5")
+httpd = Server("IIS8.5 - ASP.NET Web Forms App")
 httpd.inBoundary = dmz
 httpd.levels = [0,1]
 httpd.OS = "Windows Server 2012 R2"
 httpd.port = 80
 httpd.protocol = "HTTP"
 
-dbengine = Server("MSSQL11")
-dbengine.inBoundary = internal_network
-dbengine.levels = [0,1]
-dbengine.OS = "Windows Server 2012 R2"
-dbengine.port = 1433
-dbengine.protocol = "TCP"
+db = Datastore("MSSQL11 - IntranetWebDB")
+db.inBoundary = internal_network
+db.levels = [0,1]
+db.isSQL = True
+db.type = DatastoreType.SQL
+db.OS = "Windows Server 2012 R2"
+db.port = 1433
+db.protocol = "TCP"
+db.storesPII = True
+db.storesLogData = True
+db.storesSensitiveData = True
 
-Db = Datastore("IntranetWebDB")
-Db.inBoundary = internal_network
-Db.levels = [0,1]
-Db.isSQL = True
-Db.type = DatastoreType.SQL
-Db.storesPII = True
-Db.storesLogData = True
-Db.storesSensitiveData = True
-
-token_user_identity = Data("Token verifying user identity")
-token_user_identity.classification = Classification.SECRET
-token_user_identity.isPII = False
-token_user_identity.isStored = False
-token_user_identity.isCredentials = True
-
-
-
-user_to_httpd = Dataflow(user, httpd, "Send login credentials")
+user_to_httpd = Dataflow(user, httpd, "Send login credential")
 user_to_httpd.levels = [0,1]
-httpd_to_dbengine = Dataflow(httpd, dbengine, "Send hashed password to DB")
-httpd_to_dbengine.levels = [0,1]
-dbengine_to_httpd = Dataflow(dbengine, httpd, "Send token to verify user identity")
-
+httpd_to_db = Dataflow(httpd, db, "Query user identity")
+httpd_to_db.levels = [0,1]
+db_to_httpd = Dataflow(db, httpd, "Send user identity")
+db_to_httpd.levels = [0,1]
+httpd_to_httpd = Dataflow(httpd, httpd, "Keep session in memory")
+httpd_to_httpd.levels = [0,1]
 
 
 if __name__ == "__main__":
